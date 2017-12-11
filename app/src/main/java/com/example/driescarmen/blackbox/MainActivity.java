98c7138dea65aaf350d0e6084c33f9fa298de94b
Dries Carmen <Driezy52@hotmail.com>
2017-12-11 15:00:53 +0100

    Firebase added
    Firebase connection added. can send data to the server.

--- app/src/main/java/com/example/driescarmen/blackbox/MainActivity.java
+++ app/src/main/java/com/example/driescarmen/blackbox/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.driescarmen.blackbox;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    int num = 0;
+    public void Send(View v)
+    {
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!" + num);
+        num++;
     }
 }
